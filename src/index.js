490a473933cedbb54954d3aa077719647ce950d7
Keunrim Baek <neorimy@gmail.com>
2022-12-14 20:31:32 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,23 +4,34 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 //reducer
-const countModifier = (state, action) => {
-  console.log(state, action);
-  if (state === undefined) {
-    return { color: "yellow" };
-  }
-  let color = action.color;
-  if (action.type === "CHANGE_COLOR") {
-    return { color: { color } };
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 //store
 const countStore = createStore(countModifier);
 
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
 add.addEventListener("click", () => {
-  countStore.dispatch({ type: "CHANGE_COLOR", color: "red" });
+  countStore.dispatch({ type: ADD });
 });
 
-// console.log(countStore.getState());
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: MINUS });
+});
